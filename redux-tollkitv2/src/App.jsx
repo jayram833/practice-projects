754020e66005e10141a77db89db24c8636e5844c
jayram833 <jaypansare96@gmail.com>
2025-03-23 11:02:01 +0530

    added todolist placeholder

--- redux-tollkitv2/src/App.jsx
+++ redux-tollkitv2/src/App.jsx
@@ -1,13 +1,15 @@
+import { useSelector } from "react-redux"
 import AddTodoForm from "./AddTodoForm"
-import Todo from "./Todo"
 import TodoList from "./TodoList"
+import TodoListPlaceholder from "./TodoListPlaceholder"
 
 function App() {
+  const todoList = useSelector(state=>state.todo.todos)
   return (
     <div className="flex justify-center flex-col items-center">
         <h1 className="text-4xl font-semibold text-center">Redux ToDo List</h1>
         <AddTodoForm />
-        <TodoList />
+        {todoList.length<=0 ? <TodoListPlaceholder />:<TodoList />}
     </div>
   )
 }
---
+++ redux-tollkitv2/src/TodoListPlaceholder.jsx
@@ -0,0 +1,9 @@
+function TodoListPlaceholder() {
+    return (
+        <div className="mt-5 text-2xl bg-pink-200 w-100 h-50 flex justify-center items-center font-semibold">
+          <p>Start adding todo's</p>
+        </div>
+    )
+}
+
+export default TodoListPlaceholder
