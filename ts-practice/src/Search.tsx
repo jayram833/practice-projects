79beff172ffb5a2222dd6c54dbcbafba52af4e12
jayram833 <jaypansare96@gmail.com>
2025-04-08 00:07:33 +0530

    added fix

--- ts-practice/src/Search.tsx
+++ ts-practice/src/Search.tsx
@@ -5,7 +5,7 @@ type ResultsItem = {
     name: string
 }
 type ItemProps = {
-    item: string
+    item: ResultsItem
 }
 
 function Search() {
@@ -13,7 +13,10 @@ function Search() {
     const [resultItems, setResultItems] = useState<ResultsItem[]>([]);
 
     function handleSearch(inputQuery: string) {
-        if (inputQuery.length <= 2) return;
+        if (inputQuery.length <= 2) {
+            setResultItems([])
+            return;
+        }
         const itemsList: ResultsItem[] = data.filter(ele => ele.name.toLowerCase().includes(inputQuery));
         console.log(itemsList)
         setResultItems(itemsList)
@@ -33,7 +36,7 @@ function Search() {
                 <input value={inputQuery} onChange={e => setInputQuery(e.target.value)} className="px-2 py-1 bg-white rounded-md w-110 focus:outline-none" type="text" placeholder="Search Item" />
             </div>
             <div>
-                <ul>
+                <ul className="max-h-[500px] overflow-x-scroll">
                     {resultItems.map((item, i) => <Item key={i} item={item} />)}
                 </ul>
             </div>
@@ -46,7 +49,7 @@ function Search() {
 
 
 function Item({ item }: ItemProps) {
-    return <li className="bg-blue-600 text-white cursor-pointer hover:bg-blue-500 w-110 px-2 border-b-[0.5px] border-blue-100">{item.name}</li>
+    return <li className="bg-blue-600 text-white cursor-pointer hover:bg-blue-500 w-110 px-2 py-1 border-b-[0.5px] border-blue-100">{item.name}</li>
 }
 
 export default Search
